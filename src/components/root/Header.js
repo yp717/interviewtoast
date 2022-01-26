5500d410ad3949006202ffd20c2034ca15dd84bf
Sam Larsen-Disney <s.larsendisney@gmail.com>
2022-01-26 18:07:40 +0000

    feat(menu): added account menu

--- src/components/root/Header.js
+++ src/components/root/Header.js
@@ -2,25 +2,50 @@ import { Link } from "gatsby"
 import * as React from "react"
 import Logo from "../../assets/Logo"
 import { useAuth } from "../../context/auth-context"
+import useOutsideAlerter from "../../hooks/useOutsideAlerter"
 
 const Header = () => {
   const { user, logout } = useAuth()
+  const [menuOpen, setMenuOpen] = React.useState(false)
+  const menuRef = React.useRef(null)
+  useOutsideAlerter(menuRef, () => setMenuOpen(false))
   return (
     <div className="flex justify-between items-center max-w-6xl mx-auto px-2 py-2 md:pt-6">
       <Link
         to={user ? "/dashboard" : "/"}
-        className="flex items-center font-bold group text-orange-400 hover:text-white"
+        className="flex items-center font-bold group hover:text-gray-900"
       >
         <h1 className="text-xl">Interview</h1>
         <Logo className="h-8 w-8 group-hover:animate-bounce" alt="Toast" />
       </Link>
       {user && (
-        <div className="flex items-center space-x-1">
-          <p>{user.displayName}</p>
-          <img src={user.photoURL} className="h-8 w-8 rounded-full" />
-          <button onClick={logout} className="btn-primary">
-            Logout
+        <div className="relative">
+          <button
+            onClick={() => setMenuOpen(true)}
+            className="flex items-center space-x-1"
+          >
+            <div className={`p-1 rounded-t ${menuOpen ? "1 bg-gray-600" : ""}`}>
+              <img
+                src={user.photoURL}
+                className={`h-8 w-8 rounded-full relative z-30 `}
+              />
+            </div>
           </button>
+          {menuOpen && (
+            <div
+              ref={menuRef}
+              className="absolute top-0 z-20 right-0 mt-10 w-48 rounded-b rounded-tl text-gray-100 bg-gray-600 shadow-lg space-y-2 divide-y pt-2 divide-gray-700 "
+            >
+              <Link to="/dashboard" className="hover:text-orange-400 p-2">
+                Dashboard
+              </Link>
+              <div className="p-2">
+                <button onClick={logout} className="btn-primary w-full">
+                  Logout
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
