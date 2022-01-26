3313bda9b456c7fbaa60db8e901d16e90f040e24
Sam Larsen-Disney <s.larsendisney@gmail.com>
2022-01-26 18:20:58 +0000

    fix(lint)

--- src/hooks/useOutsideAlerter.js
+++ src/hooks/useOutsideAlerter.js
@@ -1,19 +1,18 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react"
 
 function useOutsideAlerter(ref, cb) {
-    useEffect(() => {
-        
-        function handleClickOutside(event) {
-            if (ref.current && !ref.current.contains(event.target)) {
-                cb();
-            }
-        }
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        cb()
+      }
+    }
 
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [ref]);
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [ref])
 }
 
 export default useOutsideAlerter
