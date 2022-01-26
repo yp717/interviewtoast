c45d7df6d837fd95c40c34daba9630c2b4dcec5b
Yannis Panagis <yannis.panagis1998@gmail.com>
2022-01-26 19:48:30 +0000

    feat(enable-cors): enabled cors

--- server/index.js
+++ server/index.js
@@ -5,6 +5,18 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
+// enable CORS
+app.use(function (_, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Credentials", true)
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma"
+  )
+  next()
+})
+
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
