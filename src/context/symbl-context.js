df662e5fa657a1916efc3a339e8099e5309835c5
Sam Larsen-Disney <s.larsendisney@gmail.com>
2022-02-04 01:19:22 +0000

    feat(symbl): transcription from symbl working

---
+++ src/context/symbl-context.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useContext, useEffect } from "react"
+import { useAuth } from "./auth-context"
+import { Symbl } from "symbl-chime-adapter"
+import Toast from "../components/root/Toast"
+import { useToast } from "./toast-context"
+
+const SymblContext = React.createContext()
+
+var symbl
+
+export const SymblProvider = ({ meetingID, ...props }) => {
+  const { user } = useAuth()
+  const { fireToast } = useToast()
+  useEffect(() => {
+    ;(async () => {
+      const res = await fetch("/api/access-token")
+      const { accessToken } = await res.json()
+
+      const config = {
+        attendeeId: user.uid,
+        meetingId: meetingID,
+        userName: user.uid,
+        meeting: meetingID,
+      }
+      Symbl.ACCESS_TOKEN = accessToken
+      symbl = new Symbl(config)
+      const transcriptHandler = {
+        onTranscriptCreated: transcript => {
+          console.log("On transcript created", transcript)
+          fireToast(
+            <Toast duration={3500} title={`Transcript Update`} status="speaker">
+              <p>{transcript.message}</p>
+            </Toast>
+          )
+        },
+      }
+      symbl.subscribeToTranscriptEvents(transcriptHandler)
+      symbl.start()
+    })()
+  }, [])
+  return <SymblContext.Provider value={{}} {...props} />
+}
+
+export const useSymbl = () => useContext(SymblContext)
+
+export default SymblContext
