0c533bcfa43a03eb394871d4cb37107c2899d3fc
Yannis Panagis <yannis.panagis1998@gmail.com>
2022-02-04 19:51:42 +0000

    feat(video-chat-ui): added basic video chat ui

--- src/components/meeting/MeetingWindow.js
+++ src/components/meeting/MeetingWindow.js
@@ -3,6 +3,8 @@ import * as React from "react"
 import MediaPlayer from "./MediaPlayer"
 
 import { SymblProvider } from "../../context/symbl-context"
+import { CameraIcon, MicrophoneIcon } from "@heroicons/react/solid"
+import { LogoutIcon } from "@heroicons/react/outline"
 
 const MeetingWindow = ({
   meetingID,
@@ -38,8 +40,48 @@ const MeetingWindow = ({
           )
         })}
       </div>
+
+      <MeetingMenu />
       {/* </SymblProvider> */}
     </div>
+  )
+}
+
+const MeetingMenu = () => {
+  return (
+    <div className="w-full h-18 bg-gray-900 opacity-70">
+      <div className="flex flex-row items-center justify-center space-x-6 px-4">
+        <ButtonWithLabelBelow
+          Icon={MicrophoneIcon}
+          label="Mute"
+          onClick={() => console.log("Should mute")}
+        />
+
+        <ButtonWithLabelBelow
+          Icon={CameraIcon}
+          label="Camera"
+          onClick={() => console.log("Should Turn Off Camera")}
+        />
+
+        <ButtonWithLabelBelow
+          Icon={LogoutIcon}
+          label="Leave"
+          onClick={() => console.log("Should Leave")}
+        />
+      </div>
+    </div>
+  )
+}
+
+const ButtonWithLabelBelow = ({ Icon, label, onClick }) => {
+  return (
+    <button
+      className="flex flex-col justify-center hover:bg-gray-600 w-16 h-14 rounded-md my-2"
+      onClick={onClick}
+    >
+      <Icon className="w-6 h-6 mx-auto" />
+      <p className="mx-auto">{label}</p>
+    </button>
   )
 }
 
