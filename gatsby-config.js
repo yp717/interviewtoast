c097dce67b28da9682109b4a286378298faad2bc
Sam Larsen-Disney <s.larsendisney@gmail.com>
2022-02-05 13:01:48 +0000

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,9 @@ module.exports = {
     author: `Yannis Panagis & Sam Larsen Disney`,
     description: `Your new favourite interview preparation, assessment and feedback tool`,
   },
-  plugins: ["gatsby-plugin-postcss", `gatsby-plugin-react-helmet-async`],
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet-async`,
+    `gatsby-plugin-gatsby-cloud`,
+  ],
 }
