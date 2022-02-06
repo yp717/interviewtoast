322883b456356ecfb29b68f7fa77902c39705a7e
Yannis Panagis <yannis.panagis1998@gmail.com>
2022-02-06 20:02:34 +0000

    feat(404): created 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,42 @@
 import * as React from "react"
-import { Link } from "gatsby"
 
+import { Link } from "gatsby"
+import { m } from "framer-motion"
+
+import babyYoda from "../../static/baby-yoda-bg-removebg-preview.png"
+
+import Layout from "../components/root/Layout"
+import Logo from "../assets/Logo"
 const NotFoundPage = () => {
   return (
-    <main>
-      <h1>You're lost</h1>
-      <Link to="/">Go home</Link>.
-    </main>
+    <Layout>
+      {/*
+       */}
+      <h1 className="text-6xl font-bold mt-20 mb-6">
+        Error 404. This is not the Toast you are looking for.
+      </h1>
+      <Link className="btn-primary font-bold text-2xl py-2" to="/">
+        Go home?
+      </Link>
+      <div className="w-full h-full items-center justify-center pb-16 relative">
+        <img src={babyYoda} className="mx-auto z-10" />
+        <div className="absolute top-1/2 left-1/2">
+          <m.div
+            initial={{ rotate: 0 }}
+            animate={{ rotate: 360 }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "loop",
+              duration: 5,
+              ease: "linear",
+            }}
+            className="w-42 h-42 relative left-60 mb-12"
+          >
+            <Logo className="h-10 w-10 absolute" />
+          </m.div>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
